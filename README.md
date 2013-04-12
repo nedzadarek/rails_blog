66bce95344d95a9737da8c33327b5cdad028f8d7
Darek Nedza <nedzadarek@gmail.com>
2013-04-12 23:38:38 +0200

    Working. Not modified.

---
+++ README.md
@@ -0,0 +1,1 @@
+I've just followed this tutorial: http://guides.rubyonrails.org/getting_started.html It may contain minor errors but it's working.
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '643f68aebe6e0f9ec1634f8e21cb8468c1008a3e1edd2cfc2b1e675ac5e98ec1644f1305e8c3eb66c2b770c41c5e1f6f63aec9dff7e7b0f98e1308f52c82e96d'
